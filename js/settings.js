5c378bfce457d4e9f7366ab99b1637aa25877a34
André  Sobral <50216631+Slamrap@users.noreply.github.com>
2020-01-29 19:35:45 +0000

    Minor bug fixes

--- js/settings.js
+++ js/settings.js
@@ -1,23 +1,22 @@
-// Get the modal
+// Pop up window
 var settings = document.getElementsByClassName("settings")[0];
 
-// Get the button that opens the modal
-var btn = document.getElementById("settings_btn");
+// Buttons for openning settings menu
+var settings_btn = document.getElementById("settings_btn");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// Close settings button
+var close_btn = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
+
+settings_btn.onclick = function() {
     settings.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+close_btn.onclick = function() {
     settings.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
+// Close settings menu when clicking outside of it
 window.onclick = function(event) {
   if (event.target == settings) {
     settings.style.display = "none";
